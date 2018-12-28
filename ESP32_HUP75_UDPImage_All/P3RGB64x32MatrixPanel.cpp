48977fe7ff920b69ace5ecc6c296b0aac4a7ceac
Hess Patrick <0812@domsac.de>
2018-12-28 19:06:50 +0100

    New ESP32 64x64 project

---
+++ ESP32_HUP75_UDPImage_All/P3RGB64x32MatrixPanel.cpp
@@ -0,0 +1,140 @@
+#include "P3RGB64x32MatrixPanel.h"
+
+volatile SemaphoreHandle_t P3RGB64x32MatrixPanel::timerSemaphore;
+P3RGB64x32MatrixPanel* P3RGB64x32MatrixPanel ::singleton;
+
+void IRAM_ATTR P3RGB64x32MatrixPanel::onTimer() {
+  portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
+  portENTER_CRITICAL_ISR(&timerMux);
+
+  singleton->draw();
+
+  portEXIT_CRITICAL_ISR(&timerMux);
+  xSemaphoreGiveFromISR(timerSemaphore, NULL);
+}
+
+void P3RGB64x32MatrixPanel::begin() {
+  singleton = this;
+
+  pinMode(pinR1, OUTPUT);
+  pinMode(pinG1, OUTPUT);
+  pinMode(pinB1, OUTPUT);
+  pinMode(pinR2, OUTPUT);
+  pinMode(pinG2, OUTPUT);
+  pinMode(pinB2, OUTPUT);
+
+  pinMode(pinLAT, OUTPUT);
+  pinMode(pinCLK, OUTPUT);
+  pinMode(pinOE,  OUTPUT);
+
+  pinMode(pinA, OUTPUT);
+  pinMode(pinB, OUTPUT);
+  pinMode(pinC, OUTPUT);
+  pinMode(pinD, OUTPUT);
+
+  digitalWrite(pinLAT, LOW);
+  digitalWrite(pinCLK, LOW);
+  digitalWrite(pinOE, HIGH);
+
+  timerSemaphore = xSemaphoreCreateBinary();
+  timer = timerBegin(0, 80, true);
+  timerAttachInterrupt(timer, &onTimer, true);
+  timerAlarmWrite(timer, 60, true);
+  timerAlarmEnable(timer);
+}
+
+void P3RGB64x32MatrixPanel::stop() {
+  if (timer) {
+    timerDetachInterrupt(timer);
+    timerEnd(timer);
+  }
+}
+
+uint16_t P3RGB64x32MatrixPanel::colorHSV(long hue, uint8_t sat, uint8_t val) {
+  uint8_t  r, g, b, lo;
+  uint16_t s1, v1;
+
+  // Hue ( 0 - 1535 )
+  hue %= 1536;
+  if (hue < 0) hue += 1536;
+  lo = hue & 255;          // Low byte  = primary/secondary color mix
+  switch (hue >> 8) {      // High byte = sextant of colorwheel
+    case 0 : r = 255     ; g =  lo     ; b =   0     ; break; // R to Y
+    case 1 : r = 255 - lo; g = 255     ; b =   0     ; break; // Y to G
+    case 2 : r =   0     ; g = 255     ; b =  lo     ; break; // G to C
+    case 3 : r =   0     ; g = 255 - lo; b = 255     ; break; // C to B
+    case 4 : r =  lo     ; g =   0     ; b = 255     ; break; // B to M
+    default: r = 255     ; g =   0     ; b = 255 - lo; break; // M to R
+  }
+
+  s1 = sat + 1;
+  r  = 255 - (((255 - r) * s1) >> 8);
+  g  = 255 - (((255 - g) * s1) >> 8);
+  b  = 255 - (((255 - b) * s1) >> 8);
+
+  v1 = val + 1;
+  r = (r * v1) >> 11;
+  g = (g * v1) >> 11;
+  b = (b * v1) >> 11;
+
+  return color555(r, g, b);
+}
+
+void P3RGB64x32MatrixPanel::drawPixel(int16_t x, int16_t y, uint16_t color) {
+  if (x < 0 || x >= DWIDTH || y < 0 || y >= DHEIGHT) return;
+  int16_t idx = x + y * DWIDTH;
+  drawBuffer()[idx] = color;
+}
+
+uint16_t P3RGB64x32MatrixPanel::getPixel(int16_t x, int16_t y) {
+  if (x < 0 || x >= DWIDTH || y < 0 || y >= DHEIGHT) 0;
+  int16_t idx = x + y * DWIDTH;
+  return drawBuffer()[idx];
+}
+
+void IRAM_ATTR P3RGB64x32MatrixPanel::draw() {
+  static byte pheight = DHEIGHT-1;
+  static byte pHeightShift = DHEIGHT;
+  static byte cnt = 30;
+  static byte y = 15;
+  static uint32_t out = 0;
+  y = (y + 1) % DISPLAYDEVIDER;
+
+  if (y == 0)
+    cnt = (cnt + 1) % 31; // 31 must be skipped
+
+  byte cmp = (cnt >> 4) | ((cnt >> 2) & 0x2) | (cnt & 0x4) | ((cnt << 2) & 0x8) | ((cnt << 4) & 0x10);
+
+  for (int x = 0; x < DWIDTH; x++) {
+    bool r1, b1, g1, r2, g2, b2;
+    uint16_t c = matrixbuff[x + y * DWIDTH];
+    r1 = (c & pheight) > cmp;
+    g1 = ((c >>  5) & pheight) > cmp;
+    b1 = ((c >> 10) & pheight) > cmp;
+    c = matrixbuff[x + (y + 16) * DWIDTH];
+    r2 = (c & pheight) > cmp;
+    g2 = ((c >>  5) & pheight) > cmp;
+    b2 = ((c >> 10) & pheight) > cmp;
+
+    REG_WRITE(GPIO_OUT_REG, out |
+       ((uint32_t)r1 << pinR1) |
+       ((uint32_t)g1 << pinG1) |
+       ((uint32_t)b1 << pinB1) |
+       ((uint32_t)r2 << pinR2) |
+       ((uint32_t)g2 << pinG2) |
+       ((uint32_t)b2 << pinB2));
+
+    REG_WRITE(GPIO_OUT_W1TS_REG, (1 << pinCLK));
+  }
+  //REG_WRITE(GPIO_OUT_W1TC_REG, (1 << pinCLK));
+
+  REG_WRITE(GPIO_OUT1_W1TS_REG, (1 << (pinOE - pHeightShift)) | (1 << (pinLAT - pHeightShift)));
+
+  out = ((y & 1) << pinA) | ((y & 2) << (pinB - 1)) |
+        ((y & 4) << (pinC - 2)) | ((y & 8) << (pinD - 3));
+  REG_WRITE(GPIO_OUT_REG, out);
+
+  for (int x = 0; x < 8; x++) NOP(); // to wait latch and row switch
+
+  REG_WRITE(GPIO_OUT1_W1TC_REG, (1 << (pinOE - pHeightShift)) | (1 << (pinLAT - pHeightShift)));
+}
---
+++ ESP32_HUP75_UDPImage_All/P3RGB64x32MatrixPanel.h
@@ -0,0 +1,84 @@
+#ifndef _ESP32_P3_RGB_64_32_MATRIX_PANEL
+#define _ESP32_P3_RGB_64_32_MATRIX_PANEL
+#define DWIDTH 128 // 128
+#define DHEIGHT 32  // 32
+#define DISPLAYDEVIDER 16  //16
+#include <vector>
+#include <array>
+#include "Adafruit_GFX.h"
+
+class P3RGB64x32MatrixPanel : public Adafruit_GFX {
+  public:
+    P3RGB64x32MatrixPanel(uint8_t _pinR1, uint8_t _pinG1, uint8_t _pinB1, uint8_t _pinR2, uint8_t _pinG2, uint8_t _pinB2, uint8_t _pinCLK, uint8_t _pinLAT, uint8_t _pinOE, uint8_t _pinA, uint8_t _pinB, uint8_t _pinC, uint8_t _pinD, bool _doubleBuffer = false)
+      : Adafruit_GFX(DWIDTH, DHEIGHT), pinR1(_pinR1), pinG1(_pinG1), pinB1(_pinB1), pinR2(_pinR2), pinG2(_pinG2), pinB2(_pinB2), pinCLK(_pinCLK), pinLAT(_pinLAT), pinOE(_pinOE), pinA(_pinA), pinB(_pinB), pinC(_pinC), pinD(_pinD), doubleBuffer(_doubleBuffer) {
+      initMatrixBuff();
+    }
+    P3RGB64x32MatrixPanel(bool _doubleBuffer = false)
+      : Adafruit_GFX(DWIDTH, DHEIGHT), doubleBuffer(_doubleBuffer) {
+      initMatrixBuff();
+    }
+    void begin(void);
+    void stop(void);
+    virtual void drawPixel(int16_t x, int16_t y, uint16_t color);
+    virtual uint16_t getPixel(int16_t x, int16_t y);
+
+    uint16_t color444(uint8_t r, uint8_t g, uint8_t b) { return ((g & 0xf) << 1) | ((uint16_t)(b & 0xf) << 6) | ((uint16_t)(r & 0xf) << 11); }
+    uint16_t color555(uint8_t r, uint8_t g, uint8_t b) { return (g&0x1f) | ((uint16_t)(b & 0x1f) << 5) | ((uint16_t)(r & 0x1f) << 10); }
+    uint16_t colorHSV(long hue, uint8_t sat, uint8_t val);
+
+    void swapBuffer() {
+      matrixbuff = drawBuffer();
+    }
+
+    uint16_t* matrixbuff;
+    std::vector<std::array<uint16_t, DWIDTH*DHEIGHT>> _matrixbuff;
+
+    void copyBuffer() {
+      if (!doubleBuffer) return;
+      if (matrixbuff == _matrixbuff[0].data())
+        _matrixbuff[0] = _matrixbuff[1];
+      else
+        _matrixbuff[1] = _matrixbuff[0];
+    }
+
+  private:
+    void initMatrixBuff() {
+      _matrixbuff.resize(doubleBuffer ? 2 : 1);
+      matrixbuff = _matrixbuff[0].data();
+    }
+    static void IRAM_ATTR onTimer(void);
+    void IRAM_ATTR draw();
+
+    uint16_t* drawBuffer() {
+      if (!doubleBuffer) return _matrixbuff[0].data();
+      if (matrixbuff == _matrixbuff[0].data())
+        return _matrixbuff[1].data();
+      else
+        return _matrixbuff[0].data();
+    }
+
+    hw_timer_t* timer;
+
+    uint8_t pinR1 = 25;
+    uint8_t pinG1 = 26;
+    uint8_t pinB1 = 27;
+    uint8_t pinR2 = 21;
+    uint8_t pinG2 = 22;
+    uint8_t pinB2 = 23;
+
+    uint8_t pinCLK = 15;
+    uint8_t pinLAT = 32;
+    uint8_t pinOE = 33;
+
+    uint8_t pinA = 12;
+    uint8_t pinB = 16;
+    uint8_t pinC = 17;
+    uint8_t pinD = 18;
+
+    bool doubleBuffer;
+
+    static volatile SemaphoreHandle_t timerSemaphore;
+    static P3RGB64x32MatrixPanel *singleton;
+};
+
+#endif
